0e4d8047aa98ff9cf8a4b57fb3207ea6c08524c7
Sajanader <sajanader93@gmail.com>
2021-01-18 21:23:48 +0200

    lab_37

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Odometer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      odo: "0000",
+    };
+  }
+  changeOdometer(n) {
+    let num = Number(this.state.odo) + n;
+    if (num > 9999) {
+      this.setState({ odo: "0000" });
+      return;
+    }
+    if (num < 10) {
+      this.setState({ odo: "000" + num });
+      return;
+    }
+    if (num < 100) {
+      this.setState({ odo: "00" + num });
+      return;
+    }
+    if (num < 1000) {
+      this.setState({ odo: "0" + num });
+      return;
+    }
+    this.setState({ odo: num });
+  }
+  render() {
+    return (
+      <>
+        <button className="odo" onClick={() => this.changeOdometer(1)}>add 1</button>
+        <button className="odo" onClick={() => this.changeOdometer(10)}>add 10</button>
+        <button className="odo" onClick={() => this.changeOdometer(100)}>add 100</button>
+        <button className="odo" onClick={() => this.changeOdometer(1000)}>add 1000</button>
+        <div className="odo">
+          <h3>{this.state.odo}</h3>
+        </div>
+      </>
+    );
+  }
 }
 
+
+
+
+function Main() {
+  return (
+    <body>
+      <div className="body">
+        <h1> {lab()}</h1>
+        <p>Welcome to React world!!! </p>
+      </div>
+    </body>
+  );
+}
+function Footer() {
+  return (
+    <footer className="App">
+      <p>Copy Right @</p>
+      <span>saja</span>
+    </footer>
+  );
+}
+function App() {
+  return (
+    <>
+    
+      <Main />
+      <Odometer />
+      <Footer />
+    </>
+  );
+}
+function lab() {
+  return "Lab_37";
+}
 export default App;
